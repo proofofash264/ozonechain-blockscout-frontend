d18c17aca58de45525d7b63020fde078dbc3524a
ashumehta26 <mehta.ashu264@gmail.com>
2026-08-09 13:12:52 +0530

    Update homepage stats configuration and enhance data fetching
    - Modified the .env.example file to include new statistics variables for total supply, platform TVL, and user TVL.
    - Updated the ENVS.md documentation to reflect changes in the homepage stats configuration.
    - Refactored the useOzoneBalancesTotalsQuery hook to align with the new data structure.
    - Adjusted the Stats component to utilize the updated balances data, improving the display of total supply and TVL metrics.
    - Standardized import statements across multiple files for consistency.

--- src/slices/home/hooks/useOzoneBalancesTotalsQuery.ts
+++ src/slices/home/hooks/useOzoneBalancesTotalsQuery.ts
@@ -1,12 +1,14 @@
 // SPDX-License-Identifier: LicenseRef-Blockscout
 
-import { useQuery } from '@tanstack/react-query';
+import { useQuery } from "@tanstack/react-query";
 
-import useFetch from 'src/api/hooks/useFetch';
+import useFetch from "src/api/hooks/useFetch";
 
 export type OzoneBalancesTotalsData = {
-  contractsTotalBalance: string;
-  eoasTotalBalance: string;
+  circulatingSupply: string;
+  platformTvlBalance: string;
+  userTvlBalance: string;
+  totalSupply: string;
   lastSynced: string;
 };
 
@@ -18,24 +20,26 @@ export type OzoneBalancesTotalsResponse = {
 };
 
 const OZONE_BALANCES_TOTALS_URL =
-  'https://chain-metrics-backend.ozonescan.com/api/v1/addresses/balances/totals';
+  "https://chain-metrics-backend.ozonescan.com/api/v1/addresses/balances/totals";
 
 export default function useOzoneBalancesTotalsQuery() {
   const fetch = useFetch();
 
   return useQuery<OzoneBalancesTotalsResponse>({
-    queryKey: [ 'ozone_balances_totals' ],
-    queryFn: async() =>
+    queryKey: ["ozone_balances_totals"],
+    queryFn: async () =>
       fetch(OZONE_BALANCES_TOTALS_URL) as Promise<OzoneBalancesTotalsResponse>,
     placeholderData: {
       success: true,
-      message: 'Total balances retrieved successfully',
+      message: "Total balances retrieved successfully",
       data: {
-        contractsTotalBalance: '-',
-        eoasTotalBalance: '-',
-        lastSynced: '',
+        circulatingSupply: "-",
+        platformTvlBalance: "-",
+        userTvlBalance: "-",
+        totalSupply: "-",
+        lastSynced: "",
       },
-      timestamp: '',
+      timestamp: "",
     },
     refetchOnMount: false,
   });
